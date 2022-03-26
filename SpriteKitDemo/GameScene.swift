c2a45bde8afd6b567316dc5ce17884a508170711
Thomas George <private@thomas-george.com>
2022-03-26 14:32:38 +0100

    Basic Actions

--- SpriteKitDemo/GameScene.swift
+++ SpriteKitDemo/GameScene.swift
@@ -16,6 +16,8 @@ class GameScene: SKScene {
     let whiteBox = SKSpriteNode(color: UIColor.white, size: CGSize(width: 100, height: 100))
     
     override func didMove(to view: SKView) {
+        // MARK: - Nodes (Sprites)
+        
         addChild(self.myFirstNode)
         
         self.myFirstSpriteNode.position = CGPoint(x: frame.midX, y: frame.midY)
@@ -32,6 +34,45 @@ class GameScene: SKScene {
         
         self.whiteBox.position = CGPoint(x: self.myFirstSpriteNode.size.width / 2, y: self.myFirstSpriteNode.size.height / 2)
         self.myFirstSpriteNode.addChild(self.whiteBox)
+        
+        // MARK: - Physics
+        
+        
+    }
+    
+    // MARK: - SKAction
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // Single action
+//         myFirstTexturedNode.run(SKAction.move(to: CGPoint(x: myFirstSpriteNode.size.width, y: myFirstSpriteNode.size.height), duration: 2.0))
+//         blueBox.run(SKAction.rotate(byAngle: .pi, duration: 2.0))
+        
+        // Run action with completion handler
+        myFirstTexturedNode.run(SKAction.move(to: CGPoint(x: myFirstSpriteNode.size.width, y: myFirstSpriteNode.size.height), duration: 2.0)) {
+            self.myFirstTexturedNode.position = CGPoint.zero
+        }
+        
+        if !blueBox.hasActions() {
+            // Repeat an action forever
+//            blueBox.run(SKAction.repeatForever(SKAction.rotate(byAngle: .pi, duration: 2.0)))
+            
+            // Run multiple actions at the same time
+//            blueBox.run(SKAction.group([
+//                SKAction.rotate(byAngle: .pi, duration: 2.0),
+//                SKAction.scale(by: 0.6, duration: 2.0)
+//            ]))
+            
+            // Run multiple actions one by one
+            blueBox.run(SKAction.sequence([
+                SKAction.rotate(byAngle: .pi, duration: 2.0),
+                SKAction.scale(by: 0.6, duration: 2.0)
+            ]))
+            
+            
+        } else {
+            // Remove actions
+            blueBox.removeAllActions()
+        }
     }
     
 }
